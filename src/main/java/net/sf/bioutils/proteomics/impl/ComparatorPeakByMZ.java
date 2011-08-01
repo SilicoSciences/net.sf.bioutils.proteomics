a7e1f536edbffa8932cb187a3120337a3bba18ec
Kerner <alex.kerner.24@googlemail.com>
2011-08-01 11:13:36 +0200

    Change/ Fix ComparatorPeak*.

--- src/main/java/net/sf/bioutils/proteomics/impl/ComparatorPeakByMZ.java
+++ src/main/java/net/sf/bioutils/proteomics/impl/ComparatorPeakByMZ.java
@@ -3,10 +3,11 @@ package net.sf.bioutils.proteomics.impl;
 import java.util.Comparator;
 
 import net.sf.bioutils.proteomics.Peak;
+import net.sf.kerner.utils.collections.impl.ComaratorNull;
 
-public class ComparatorPeakByMZ implements Comparator<Peak> {
+public class ComparatorPeakByMZ extends ComaratorNull<Peak> implements Comparator<Peak> {
 
-	public int compare(Peak o1, Peak o2) {
+	public int compareNonNull(Peak o1, Peak o2) {
 
 		return Double.compare(o1.getMZ(), o2.getMZ());
 

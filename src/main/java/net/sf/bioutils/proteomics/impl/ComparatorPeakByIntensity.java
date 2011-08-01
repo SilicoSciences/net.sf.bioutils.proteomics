2857bc34c2a5c81baee60e9099eb0aa04809f037
Kerner <alex.kerner.24@googlemail.com>
2011-08-01 16:56:19 +0200

    Minor.

--- src/main/java/net/sf/bioutils/proteomics/impl/ComparatorPeakByIntensity.java
+++ src/main/java/net/sf/bioutils/proteomics/impl/ComparatorPeakByIntensity.java
@@ -3,9 +3,9 @@ package net.sf.bioutils.proteomics.impl;
 import java.util.Comparator;
 
 import net.sf.bioutils.proteomics.Peak;
-import net.sf.kerner.utils.collections.impl.ComaratorNull;
+import net.sf.kerner.utils.collections.impl.ComparatorNull;
 
-public class ComparatorPeakByIntensity extends ComaratorNull<Peak> implements Comparator<Peak> {
+public class ComparatorPeakByIntensity extends ComparatorNull<Peak> implements Comparator<Peak> {
 
 	public int compareNonNull(Peak o1, Peak o2) {
 		return Double.compare(o1.getIntensity(), o2.getIntensity());
--- src/main/java/net/sf/bioutils/proteomics/impl/ComparatorPeakByMZ.java
+++ src/main/java/net/sf/bioutils/proteomics/impl/ComparatorPeakByMZ.java
@@ -3,9 +3,9 @@ package net.sf.bioutils.proteomics.impl;
 import java.util.Comparator;
 
 import net.sf.bioutils.proteomics.Peak;
-import net.sf.kerner.utils.collections.impl.ComaratorNull;
+import net.sf.kerner.utils.collections.impl.ComparatorNull;
 
-public class ComparatorPeakByMZ extends ComaratorNull<Peak> implements Comparator<Peak> {
+public class ComparatorPeakByMZ extends ComparatorNull<Peak> implements Comparator<Peak> {
 
 	public int compareNonNull(Peak o1, Peak o2) {
 

97052eef93db262cc1008d0da0893d8ce20aec54
Kerner <alexanderkerner24@gmail.com>
2012-07-30 17:15:43 +0200

    Stuff.

--- src/main/java/net/sf/bioutils/proteomics/PeakView.java
+++
@@ -1,8 +0,0 @@
-package net.sf.bioutils.proteomics;
-
-import net.sf.kerner.utils.Transformer;
-import net.sf.kerner.utils.collections.list.TransformerList;
-
-public interface PeakView<V> extends Transformer<Peak, V>, TransformerList<Peak, V> {
-
-}

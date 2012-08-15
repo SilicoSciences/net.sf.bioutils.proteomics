cd609c7bf67d1154f7273e189b3980c0d28af293
Kerner <alexanderkerner24@gmail.com>
2012-08-15 09:08:17 +0200

    Change default accuracy.

--- src/main/java/net/sf/bioutils/proteomics/impl/PeakFilterByMZ.java
+++ src/main/java/net/sf/bioutils/proteomics/impl/PeakFilterByMZ.java
@@ -6,19 +6,19 @@ import net.sf.kerner.utils.math.UtilMath;
 
 public class PeakFilterByMZ implements Filter<Peak> {
 
-    public static int DEFAULT_ACCURACY = 4;
+    public static int DEFAULT_ACCURACY = 1;
+
+    private final int accuracy;
 
     private final double mz;
 
-    private final int accuracy;
+    public PeakFilterByMZ(final double mz) {
+        this(mz, DEFAULT_ACCURACY);
+    }
 
     public PeakFilterByMZ(final double mz, final int accuracy) {
         this.accuracy = accuracy;
         this.mz = mz;
-    }
-
-    public PeakFilterByMZ(final double mz) {
-        this(mz, DEFAULT_ACCURACY);
     }
 
     @Override

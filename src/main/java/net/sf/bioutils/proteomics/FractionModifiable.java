ac9e7808354368f3a7e55d9e30f40baf9084d30b
Kerner <alexanderkerner24@gmail.com>
2013-02-16 19:07:27 +0100

    API-Break: Add more setters().

--- src/main/java/net/sf/bioutils/proteomics/FractionModifiable.java
+++ src/main/java/net/sf/bioutils/proteomics/FractionModifiable.java
@@ -1,3 +1,18 @@
+/**********************************************************************
+ Copyright (c) 2013 Alexander Kerner. All rights reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ***********************************************************************/
+
 package net.sf.bioutils.proteomics;
 
 import java.util.List;
@@ -15,18 +30,24 @@ import java.util.List;
  * 
  * </p>
  * <p>
- * last reviewed 2011-11-10
+ * last reviewed 2013-02-16
  * </p>
  * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2012-11-12
+ * @version 2013-02-16
  */
 public interface FractionModifiable extends Fraction {
 
     void addPeak(Peak peak);
 
+    void addStandard(Standard standard);
+
     void setName(String name);
 
-    
+    void setPeaks(List<? extends Peak> peaks);
+
+    void setSample(Sample sample);
+
+    void setStandards(List<? extends Standard> standards);
 
 }

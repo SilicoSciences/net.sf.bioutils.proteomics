df1236ab04f69bb2f754b359077a63afd9be4767
Kerner <alexanderkerner24@gmail.com>
2013-03-14 14:44:11 +0100

    Feature: introduce FilterPeakMaxInt.

---
+++ src/main/java/net/sf/bioutils/proteomics/impl/FilterPeakMaxInt.java
@@ -0,0 +1,34 @@
+package net.sf.bioutils.proteomics.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import net.sf.bioutils.proteomics.Peak;
+import net.sf.kerner.utils.collections.filter.Filter;
+import net.sf.kerner.utils.collections.impl.ComparatorInverter;
+
+public class FilterPeakMaxInt implements Filter<Peak> {
+
+    private final Set<Peak> set;
+
+    public FilterPeakMaxInt(final Collection<? extends Peak> peaks, final int numElements) {
+        if (peaks.size() <= numElements) {
+            set = new HashSet<Peak>(peaks);
+        } else {
+            final TreeSet<Peak> sett = new TreeSet<Peak>(new ComparatorInverter<Peak>(new ComparatorPeakByIntensity()));
+            sett.addAll(peaks);
+            final List<Peak> list = new ArrayList<Peak>(sett);
+            set = new HashSet<Peak>(list.subList(0, numElements));
+        }
+
+    }
+
+    public boolean filter(final Peak e) {
+        return set.contains(e);
+    }
+
+}

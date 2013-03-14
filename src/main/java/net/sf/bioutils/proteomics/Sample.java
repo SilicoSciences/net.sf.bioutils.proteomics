2829a149a99dd1f0cfd88744ac43a68868dd5bf6
Kerner <alexanderkerner24@gmail.com>
2013-03-14 14:58:30 +0100

    Feature: Add Sample#clone().

--- src/main/java/net/sf/bioutils/proteomics/Sample.java
+++ src/main/java/net/sf/bioutils/proteomics/Sample.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface Sample extends AnnotatableElement<Object>, Iterable<Fraction> {
 
+    Sample clone();
+
     List<Fraction> getFractions();
 
     String getName();

933951729480c902846d9bef12c33ea6f027a7e2
Kerner <alexanderkerner24@gmail.com>
2013-07-23 09:59:08 +0200

    Introduce (package)exceptions.

---
+++ src/main/java/net/sf/bioutils/proteomics/ExceptionUnknownAminoAcid.java
@@ -0,0 +1,29 @@
+package net.sf.bioutils.proteomics;
+
+public class ExceptionUnknownAminoAcid extends ExceptionRuntimeProteomics {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6342610022514210243L;
+
+    public ExceptionUnknownAminoAcid() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExceptionUnknownAminoAcid(final String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExceptionUnknownAminoAcid(final String message, final Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExceptionUnknownAminoAcid(final Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}

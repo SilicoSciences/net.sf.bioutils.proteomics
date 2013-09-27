44b33434bf0f12b7b3409c74debf3f4a4f0f2db8
Kerner <alexanderkerner24@gmail.com>
2013-09-27 11:20:59 +0200

    Introduce Provider package.

---
+++ src/main/java/net/sf/bioutils/proteomics/provider/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * TODO description
+ *
+ * <p>
+ * <b>Example:</b><br>
+ *
+ * </p>
+ * <p>
+ * <pre>
+ * TODO example
+ * </pre>
+ * </p>
+ * <p>
+ * last reviewed: 2013-09-27
+ * </p>
+ *
+ * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
+ *
+ */
+package net.sf.bioutils.proteomics.provider;

79ce3704bac44a2b2723f6ca6a539eb714f3b0f7
Kerner <alexanderkerner24@gmail.com>
2014-04-15 11:57:17 +0200

    Peak extends Cloneable<Peak>.

--- src/main/java/net/sf/bioutils/proteomics/peak/Peak.java
+++ src/main/java/net/sf/bioutils/proteomics/peak/Peak.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,18 +21,57 @@ import net.sf.bioutils.proteomics.provider.ProviderIntensity;
 import net.sf.bioutils.proteomics.provider.ProviderMz;
 import net.sf.bioutils.proteomics.provider.ProviderSample;
 import net.sf.bioutils.proteomics.sample.Sample;
+import net.sf.kerner.utils.Cloneable;
 
-public interface Peak extends ProviderIntensity, ProviderMz, ProviderFractionIndex, ProviderSample {
+/**
+ *
+ * TODO description
+ *
+ * <p>
+ * <b>Example:</b><br>
+ *
+ * </p>
+ * <p>
+ *
+ * <pre>
+ * TODO example
+ * </pre>
+ * <p>
+ * last reviewed: 2014-04-09
+ * </p>
+ *
+ * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
+ *
+ */
+public interface Peak extends ProviderIntensity, ProviderMz, ProviderFractionIndex, ProviderSample,
+        Cloneable<Peak> {
 
+    /**
+     * Creates a clone of this {@code Peak}.</br> <b>Note:</b></br>
+     * Bidirectional relationship to {@link Fraction} and its {@link Sample}:
+     * When cloning {@link Fraction} or taking over its direct reference,
+     * {@link Fraction} - {@code Peak} relationship is broken: The cloned
+     * {@code Peak} will have a reference to a {@link Fraction} and its
+     * {@link Sample}, but this {@link Fraction} does not know about the newly
+     * cloned {@code Peak}. The cloned {@code Peak} will therefore not be
+     * accessible via {@link Fraction} and its {@link Sample}!
+     */
+    @Override
     Peak clone();
 
+    /**
+     *
+     * @return {@link Fraction}, which contains this {@code Peak}; or
+     *         {@code null} if this {@code Peak} is not contained in any
+     *         {@link Fraction}
+     */
     Fraction getFraction();
 
     /**
-     * Shortcut for {@link Peak.getFraction().getIndex()}. </p> Can be helpful
+     * Shortcut for {@link Peak.getFraction().getIndex()}. </br> Can be helpful
      * for implementations of {@code Peak} which do not have a {@link Fraction}.
-     * 
-     * @return index of {@link Fraction} which contains this {@code peak}
+     *
+     * @return index of {@link Fraction} which contains this {@code Peak}
      */
     @Override
     int getFractionIndex();
@@ -40,23 +79,31 @@ public interface Peak extends ProviderIntensity, ProviderMz, ProviderFractionInd
     String getName();
 
     /**
-     * Shortcut for {@link Peak.getFraction().getSample()}. </p> Can be helpful
+     * Shortcut for {@link Peak.getFraction().getSample()}. </br> Can be helpful
      * for implementations of {@code Peak} which do not have a {@link Fraction}.
-     * 
-     * @return {@link Sample} which is assigned to this {@code peak}
+     *
+     * @return {@link Sample} which contains this {@code Peak}
      */
     @Override
     Sample getSample();
 
     /**
-     * Shortcut for {@link Peak.getFraction().getSample().getName()}. </p> Can
+     * Shortcut for {@link Peak.getFraction().getSample().getName()}. </br> Can
      * be helpful for implementations of {@code Peak} which do not have a
      * {@link Fraction} or {@link Sample}.
-     * 
+     *
      * @return {@link Sample} which is assigned to this {@code peak}
      */
+    @Override
     String getSampleName();
 
+    /**
+     * Sets the {@link Fraction} which contains this {@code Peak}. </br>
+     * Typically, this method is called by {@link Fraction} itself!
+     *
+     * @param fraction
+     *            {@link Fraction}, which contains this {@code Peak}
+     */
     void setFraction(Fraction fraction);
 
 }

a21c257e8d31fb58abbd6e09db09bab358c7cd28
Kerner <alexanderkerner24@gmail.com>
2014-04-15 11:53:04 +0200

    JavaDoc.

--- src/main/java/net/sf/bioutils/proteomics/PeakFractionated.java
+++ src/main/java/net/sf/bioutils/proteomics/PeakFractionated.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,29 @@ package net.sf.bioutils.proteomics;
 
 import net.sf.bioutils.proteomics.peak.Peak;
 
+/**
+ *
+ * Parent ion, to which a MS2 spectrum is associated.
+ *
+ * <p>
+ * <b>Example:</b><br>
+ *
+ * </p>
+ * <p>
+ *
+ * <pre>
+ * TODO example
+ * </pre>
+ *
+ * </p>
+ *
+ * <p>
+ * last reviewed: 0000-00-00
+ * </p>
+ *
+ * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
+ *
+ */
 public interface PeakFractionated extends Peak {
 
     Spectrum getSpectrum();

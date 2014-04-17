bdc5ab8defd41d16050250037146d1357494b3b7
Kerner <alexanderkerner24@gmail.com>
2014-04-17 10:19:31 +0200

    JavaDoc.

--- src/main/java/net/sf/bioutils/proteomics/fraction/FractionModifiable.java
+++ src/main/java/net/sf/bioutils/proteomics/fraction/FractionModifiable.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,44 +19,54 @@ import net.sf.bioutils.proteomics.peak.Peak;
 import net.sf.bioutils.proteomics.standard.Standard;
 
 /**
- * 
+ *
  * Extends {@link Fraction} by adding modifiability.
- * 
+ *
  * <p>
  * <b>Example:</b><br>
- * 
+ *
  * </p>
  * <p>
- * 
+ *
  * <pre>
  * TODO example
  * </pre>
- * 
+ *
  * </p>
+ *
  * <p>
- * <b>Threading:</b><br>
- * 
+ * last reviewed: 2014-04-15
  * </p>
- * <p>
- * 
- * <pre>
- * Not thread save.
- * </pre>
- * 
- * </p>
- * <p>
- * last reviewed: 0000-00-00
- * </p>
- * 
+ *
  * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
- * 
+ *
  */
 public interface FractionModifiable extends Fraction {
 
+    /**
+     * Adds given {@link Peak} to this {@code Fraction}. Calls
+     * {@link Peak#setFraction(Fraction)} with {@code this} argument on
+     * {@link Peak} to add.
+     *
+     * @param peak
+     *            {@link Peak} to add to this {@code Fraction}
+     */
     void addPeak(Peak peak);
 
     void addStandard(Standard standard);
 
+    /**
+     * Removes the first occurrence of given {@link Peak} from this
+     * {@code Fraction}'s peak list, if it is present. If this {@code Fraction}
+     * does not contain given {@link Peak}, peak list it is unchanged.</br> If
+     * given {@link Peak} was contained by this {@code Fraction},
+     * {@link Peak#setFraction(Fraction)} with {@code null} argument is called.
+     *
+     * @param peak
+     *            {@link Peak} to remove
+     * @return {@code true}, if given {@link Peak} was contained by this
+     *         {@code Fraction}; {@code false} otherwise
+     */
     boolean removePeak(Peak peak);
 
     void setName(String name);

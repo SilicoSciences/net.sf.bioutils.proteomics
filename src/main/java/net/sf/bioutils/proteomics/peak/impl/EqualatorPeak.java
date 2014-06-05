a19c9adcfa431ffcc3fd076115a9b71d28b36378
Kerner <alexanderkerner24@gmail.com>
2014-06-05 12:05:53 +0200

    Rework EqualatorPeak/ HashCalculatorPeak to be less pedantic.

--- src/main/java/net/sf/bioutils/proteomics/peak/impl/EqualatorPeak.java
+++ src/main/java/net/sf/bioutils/proteomics/peak/impl/EqualatorPeak.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,67 +15,67 @@
  ******************************************************************************/
 package net.sf.bioutils.proteomics.peak.impl;
 
-import net.sf.bioutils.proteomics.feature.Feature;
 import net.sf.bioutils.proteomics.peak.Peak;
 import net.sf.kerner.utils.equal.Equalator;
 import net.sf.kerner.utils.equal.UtilEqual;
+import net.sf.kerner.utils.math.UtilMath;
 import net.sf.kerner.utils.pair.Pair;
 
 /**
- * 
- * TODO description
- * 
- * <p>
- * <b>Example:</b><br>
- * 
- * </p>
- * <p>
- * 
- * <pre>
- * TODO example
- * </pre>
- * 
+ *
+ * Compares {@code element} with {@code object} for equality. m/z and fraction
+ * index are considered values.
+ *
+ *
  * </p>
  * <p>
  * <b>Threading:</b><br>
- * 
+ *
  * </p>
  * <p>
- * 
- * <pre>
  * Fully thread save since stateless.
- * </pre>
- * 
  * </p>
  * <p>
- * last reviewed: 2014-02-18
+ * last reviewed: 2014-06-04
  * </p>
- * 
+ *
  * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
- * 
+ *
  */
 public class EqualatorPeak implements Equalator<Peak> {
 
     /**
-     * Calculates equality based on m/z and fraction index.
+     * Compares {@code peak} with {@code object} for equality. m/z and fraction
+     * index are considered values.
+     *
+     * @param peak
+     *            a {@code Peak}
+     * @param obj
+     *            any other object
      */
     @Override
-    public boolean areEqual(final Peak element, final Object obj) {
-        if (element == null && obj != null)
+    public boolean areEqual(final Peak peak, final Object obj) {
+        if (peak == null && obj != null)
             return false;
-        if (element != null && obj == null)
+        if (peak != null && obj == null)
             return false;
-        if (element == obj)
+        if (peak == obj)
             return true;
         if (!(obj instanceof Peak))
             return false;
-        if (element instanceof Feature && !(obj instanceof Feature))
+        if (!UtilEqual.areEqual(UtilMath.round(peak.getMz(), 4),
+                UtilMath.round(((Peak) obj).getMz(), 4)))
             return false;
-        if (obj instanceof Feature && !(element instanceof Feature))
+        if (peak.getFraction() == null && ((Peak) obj).getFraction() != null) {
             return false;
-        if (!UtilEqual.areEqual(element.getMz(), ((Peak) obj).getMz()))
+        }
+        if (peak.getFraction() != null && ((Peak) obj).getFraction() == null) {
             return false;
-        if (!UtilEqual.areEqual(element.getFractionIndex(), ((Peak) obj).getFractionIndex()))
+        }
+        if (peak.getFraction() == null && ((Peak) obj).getFraction() == null) {
+            return true;
+        }
+        if (!UtilEqual.areEqual(peak.getFractionIndex(), ((Peak) obj).getFractionIndex()))
             return false;
         return true;
     }

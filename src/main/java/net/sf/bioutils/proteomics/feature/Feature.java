8d5aa1bf068c77c793d60b036f2a55fa500cfba8
Kerner <alexanderkerner24@gmail.com>
2014-11-27 20:00:27 +0100

    Formatting.

--- src/main/java/net/sf/bioutils/proteomics/feature/Feature.java
+++ src/main/java/net/sf/bioutils/proteomics/feature/Feature.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,22 +23,6 @@ import net.sf.bioutils.proteomics.peak.Peak;
  * A {@code Feature} is a {@link ComposableElement} of {@link Peak Peaks}. It
  * represents one single peptide that was detected. One peptide is composed of
  * multiple signals (peaks). </p>
- *
- *
- * <p>
- * <b>Example:</b><br>
- *
- * </p>
- * <p>
- *
- * <pre>
- * TODO example
- * </pre>
- *
- * </p>
- * <p>
- * last reviewed: 2014-04-09
- * </p>
  *
  *
  * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>

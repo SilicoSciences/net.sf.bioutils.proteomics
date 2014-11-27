b0b7fd7d7512dc63b3fec4468a46394cf8bb644c
Kerner <alexanderkerner24@gmail.com>
2014-11-27 19:59:24 +0100

    Get away from Fraction*.

--- src/main/java/net/sf/bioutils/proteomics/UtilProteomics.java
+++ src/main/java/net/sf/bioutils/proteomics/UtilProteomics.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,6 @@ package net.sf.bioutils.proteomics;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.sf.bioutils.proteomics.fraction.Fraction;
 import net.sf.bioutils.proteomics.peak.Peak;
 import net.sf.bioutils.proteomics.peak.PeakFractionated;
 import net.sf.bioutils.proteomics.peptides.AminoAcid;
@@ -87,13 +86,12 @@ public class UtilProteomics {
     }
 
     public static boolean isAnnotatedMSMS(final Sample sample) {
-        for (final Fraction f : sample) {
-            for (final Peak p : f.getPeaks()) {
-                if (p instanceof PeakFractionated) {
-                    final PeakFractionated pf = (PeakFractionated) p;
-                    if (pf.getSpectrum() != null && !pf.getSpectrum().getPeaks().isEmpty()) {
-                        return true;
-                    }
+
+        for (final Peak p : sample.getPeaks()) {
+            if (p instanceof PeakFractionated) {
+                final PeakFractionated pf = (PeakFractionated) p;
+                if (pf.getSpectrum() != null && !pf.getSpectrum().getPeaks().isEmpty()) {
+                    return true;
                 }
             }
         }

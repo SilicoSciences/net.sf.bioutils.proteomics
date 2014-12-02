03df8dcddd4eeb57e24e2f5e3fb4dd9f585f6fca
Kerner <alexanderkerner24@gmail.com>
2014-12-02 16:16:01 +0100

    API-Break: PeakAnnotatable does not extend PeakFractionated anymore.

--- src/main/java/net/sf/bioutils/proteomics/annotation/PeakAnnotatable.java
+++ src/main/java/net/sf/bioutils/proteomics/annotation/PeakAnnotatable.java
@@ -1,35 +1,20 @@
 package net.sf.bioutils.proteomics.annotation;
 
 import net.sf.bioutils.proteomics.peak.Peak;
-import net.sf.bioutils.proteomics.peak.PeakFractionated;
 
 /**
  *
- * A {@link Peak} which is also an {@link AnnotatableElement} and a
- * {@link PeakFractionated}.
- *
- * <p>
- * <b>Example:</b><br>
- * </p>
- *
- * <p>
- *
- * <pre>
- * TODO example
- * </pre>
- *
- * </p>
- *
+ * A {@link Peak} which is also an {@link AnnotatableElement}.
  *
  *
  * <p>
- * last reviewed: 2014-05-16
+ * last reviewed: 2014-12-02
  * </p>
  *
  * @author <a href="mailto:alexanderkerner24@gmail.com">Alexander Kerner</a>
  *
  */
-public interface PeakAnnotatable extends PeakFractionated, AnnotatableElement {
+public interface PeakAnnotatable extends Peak, AnnotatableElement {
 
     @Override
     PeakAnnotatable clone();

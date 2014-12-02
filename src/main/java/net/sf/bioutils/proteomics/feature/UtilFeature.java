5765864006f23d891878ebbf4cd6d21abb55e593
Kerner <alexanderkerner24@gmail.com>
2014-12-02 16:16:56 +0100

    API-Break: Remove UtilFeature#getFractions.

--- src/main/java/net/sf/bioutils/proteomics/feature/UtilFeature.java
+++ src/main/java/net/sf/bioutils/proteomics/feature/UtilFeature.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,6 @@ import java.util.Collection;
 import java.util.List;
 
 import net.sf.bioutils.proteomics.Spectrum;
-import net.sf.bioutils.proteomics.fraction.Fraction;
 import net.sf.bioutils.proteomics.peak.ComparatorPeakByMZ;
 import net.sf.bioutils.proteomics.peak.Peak;
 import net.sf.bioutils.proteomics.peak.PeakFractionated;
@@ -60,14 +59,6 @@ public class UtilFeature {
             } else {
                 result.add(p);
             }
-        }
-        return result;
-    }
-
-    public static List<Fraction> getFractions(final List<? extends Feature> features) {
-        final List<Fraction> result = UtilList.newList();
-        for (final Feature f : features) {
-            result.add(f.getFraction());
         }
         return result;
     }

8b70e2d79e06bc69be5e8edede2517918f6d2930
Kerner <alexanderkerner24@gmail.com>
2014-12-02 16:18:26 +0100

    Implement Filter*toString.

--- src/main/java/net/sf/bioutils/proteomics/peak/FilterPeakByFractionIndexRange.java
+++ src/main/java/net/sf/bioutils/proteomics/peak/FilterPeakByFractionIndexRange.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011-2014 Alexander Kerner. All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,13 +25,13 @@ import net.sf.kerner.utils.collections.filter.Filter;
  * <b>Example:</b><br>
  * </p>
  * <p>
- * 
+ *
  * <pre>
  * TODO example
  * </pre>
- * 
+ *
  * </p>
- * 
+ *
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
  * @version 2013-10-18
  */
@@ -41,7 +41,7 @@ public class FilterPeakByFractionIndexRange implements Filter<Peak> {
 
     /**
      * Create a new {@code FilterPeakByMZRange}.
-     * 
+     *
      * @param range
      *            m/z range in which a peak's m/z must be in in order for this
      *            {@code Filter} t accept this peak
@@ -57,6 +57,6 @@ public class FilterPeakByFractionIndexRange implements Filter<Peak> {
 
     @Override
     public String toString() {
-        return "fracNrRange=" + range;
+        return "fracNr.range=" + range;
     }
 }
